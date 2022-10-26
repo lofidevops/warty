8a78be11271a39c4574c13f08daed586b15de4e4
David Seaward <david.seaward@canonical.com>
2022-10-26 19:16:39 +0100

    Experiment with clearly defined environment variables.
    Signed-off-by: David Seaward <david.seaward@canonical.com>

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
+from dataclasses import dataclass
+
+from wartlib.environment import Environment
+
+
+@dataclass
+class HelloEnvironment(Environment):
+    CANONICAL_EMAIL = ""
+
+
 print("Hello world.")
+
+E = HelloEnvironment()
+E.set_environment()
+E.validate()
+print(E.CANONICAL_EMAIL)
---
+++ wartlib/environment.py
@@ -0,0 +1,31 @@
+import os
+from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+
+@dataclass
+class Environment:
+
+    def _yield_variables(self):
+        for a in dir(self):
+            if a[0] != "_" and a.isupper():
+                yield a
+
+    def set_environment(self):
+        load_dotenv()
+        for f in self._yield_variables():
+            self.__setattr__(f, os.getenv(f, None))
+
+    def validate(self):
+        missing = []
+        for a in self._yield_variables():
+            value = self.__getattribute__(a)
+            if value is None or value == "":
+                missing.append(a)
+
+        if len(missing) == 0:
+            return True
+        else:
+            print(f"Missing: {missing}")
+            return False
